daa11c285a299fc26ae4b066b025c9332d9359c6
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-03-18 21:17:30 +0530

    client: add event for usb detection

--- events.py
+++ events.py
@@ -1,11 +1,26 @@
+import psutil
+import time
+
 def pollEvents(pollObject):
   while True:
     for fd, event in pollObject.poll():
       yield fd, event
 
 def USBEvents(serverSocket):
-  serverSocket.sendall(b"Listening to USB events~")
+
+  oldDevices = psutil.disk_partitions()
+
+  while True:    
+
+    newDevices = psutil.disk_partitions()
+
+    if(len(newDevices) > len(oldDevices)):
+      serverSocket.sendall(b"New partition mounted~")
+    elif(len(newDevices) < len(oldDevices)):
+      serverSocket.sendall(b"Partition unmounted~")
+
+    oldDevices = newDevices
+    time.sleep(0.5)
 
 def networkEvents(serverSocket):
   serverSocket.sendall(b"Listening to Network Events events~")
-  pass
