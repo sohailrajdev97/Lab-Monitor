eb9aa391e3bb8b14e6d16005f2dc3a478c74edce
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-03-24 00:49:17 +0530

    events: add event for network activity

--- events.py
+++ events.py
@@ -1,8 +1,9 @@
+import logger
 import psutil
 import socket
-import utils
+import subprocess
 import time
-import logger
+import utils
 
 def pollEvents(pollObject):
 
@@ -50,4 +51,34 @@ def USBEvents(serverSocket):
     time.sleep(0.5)
 
 def networkEvents(serverSocket):
+
   logger.log("Listening to network events", serverSocket)
+  hostname = socket.gethostname()    
+  ip = socket.gethostbyname(hostname)
+
+  dumpFilter = f"ip and host {ip} and port not 53 and ((tcp[tcpflags] & tcp-syn != 0) or udp)"
+  dumpProcess = subprocess.Popen(("tcpdump", "-l", "-nn", dumpFilter), stdout=subprocess.PIPE)
+
+  for row in iter(dumpProcess.stdout.readline, b''):
+
+    log = row.decode("utf8")
+    tokenisedLog = log.split()
+
+    src = tokenisedLog[2]
+    dest = tokenisedLog[4].rstrip(":")
+
+    try:
+      srcService = socket.getservbyport(int(src.split(".")[-1]))
+      src = f"{srcService.upper()} {'.'.join(src.split('.')[0:-1])}" 
+    except:
+      split = src.split(".")
+      src = ".".join(split[0:-1]) + ":" + split[-1]
+    
+    try:
+      destService = socket.getservbyport(int(dest.split(".")[-1]))
+      dest = f"{destService.upper()} {'.'.join(dest.split('.')[0:-1])}" 
+    except:
+      split = src.split(".")
+      src = ".".join(split[0:-1]) + ":" + split[-1]
+      
+    logger.log(f"{src} --> {dest}", serverSocket)
