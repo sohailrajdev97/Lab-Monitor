bcc3edf5a1c80d03251ef3858ffba41a9d9b4fd0
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-04-04 15:07:33 +0530

    client: add get_ip() method

--- events.py
+++ events.py
@@ -4,6 +4,17 @@ import time
 import socket
 import subprocess
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 def USBEvents(serverSocket):
 
   oldDevices = psutil.disk_partitions()
@@ -25,8 +36,7 @@ def USBEvents(serverSocket):
 def networkEvents(serverSocket):
 
   logger.log("Listening to network events", serverSocket)
-  hostname = socket.gethostname()    
-  ip = socket.gethostbyname(hostname)
+  ip = get_ip()
 
   dumpFilter = f"ip and host {ip} and port not 53 and ((tcp[tcpflags] & tcp-syn != 0) or udp)"
   dumpProcess = subprocess.Popen(("tcpdump", "-l", "-nn", dumpFilter), stdout=subprocess.PIPE)
